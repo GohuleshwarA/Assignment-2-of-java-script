01bf63667b26e5f8fe4e53cc3190e48369ad6e3b
GohuleshwarA <99191340+GohuleshwarA@users.noreply.github.com>
2022-03-10 20:13:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+var nam = "Skill Safari"
+console.log(nam)
+
+console.log(nam.length)
+console.log(nam.indexOf("a"))
+
+nam = "SKILL SAFARI"
+
+console.log(nam)
+
+//CONVERSATION BTW CAPTION AMERICA AND IRON MAN
+
+let nam1="Caption America: "
+let nam2="Iron man: "
+var str1=   "hello! I'm caption america \n " 
+var str2= "ok fine; I'm iron man \n"
+var str3="so no matter what ; i promise you,if you need us,if you need me,I'll will be there!\n"
+var str4="ok nandri \n"
+var str5="hello \n"
+var str6="ya enough let's goo "
+console.log(nam1+ str1 +nam2 + str2+nam1+str3+nam2+str4+nam1+str5+nam2+str6)
+
+//BODY MASS INDEX CALCULATOR
+
+var height=175//Apply height in cms
+var heightinMeters=height*0.01
+var metersquare=heightinMeters*heightinMeters
+
+var weight = 65 //Apply weight in kg
+
+
+const bmi=weight/metersquare
+
+console.log(bmi)
+
+
+
+
+
+
+
+
+
+
